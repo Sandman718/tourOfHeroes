af2911e4d398152c7d90be177093eed76cf6775a
Dennis sanders <92055269+Sandman718@users.noreply.github.com>
2022-02-17 00:26:39 -0800

    added hero array & mods

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import {HEROES} from '../mockHeroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -8,16 +10,28 @@ import { Hero } from '../hero';
 
 //component class file
 export class HeroesComponent implements OnInit {
-
+/*
   //hero properties of type Hero 
   hero: Hero = {
     id: 1,
   name: 'Windstorm'
   }
-
+*/
+heroes = HEROES;
+  
   constructor() { }
   
   ngOnInit(): void {
   }
-
+  
 }
+/*
+export class heroComponent implements OnInit {
+  
+  heroes = HEROES;
+  
+  constructor() {}
+  ngOnInit(): void {}
+  
+}
+*/
